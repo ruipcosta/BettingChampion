68051cee4cfb03410932c119ca623b983d543bf4
ruipcosta <ruipcosta@ua.pt>
2020-08-29 02:12:19 +0100

    bug fixes; previous games statistics

--- scripts/betchamp.py
+++ scripts/betchamp.py
@@ -4,7 +4,7 @@ import json
 from datetime import date, timedelta
 
 
-
+days = 30
     
 def getWebsite(website):
     source = requests.get(website).text
@@ -31,9 +31,9 @@ def writeJson(data, game):
 def getGameUrls(urls):
     data = date.today()
 
-    for ii in range(6):
+    for ii in range(days):
         i = 1
-        data2= data+timedelta(ii)
+        data2= data - timedelta(2)-timedelta(ii)
         while True:
             website = f"https://www.academiadasapostas.com/tips/listing/{data2}/page/{i}"
             soup = getWebsite(website)
@@ -47,6 +47,7 @@ def getGameUrls(urls):
         
     return urls
 
+
 class game():
     
     def __init__(self,url):
@@ -55,7 +56,7 @@ class game():
         if self.url != '':
             gameWebsite = getWebsite(self.url)
             self.getGameInfo(gameWebsite)
-            self.getStats(gameWebsite)
+            self.getStats(gameWebsite)        
 
 
     def getGameInfo(self, gameWebsite):
@@ -63,14 +64,31 @@ class game():
         lista = [listItem.a.span.text for listItem in headbar.find_all('li')]
         self.game = lista[3]
         self.gameLeague = lista[2]
-        print(self.game)
 
         gameData = gameWebsite.find('td', class_="stats-game-head-date")
         gameStatus = [listItem.text for listItem in gameData.find_all('li') if len(listItem.text) > 1]
-        self.finalScore = gameStatus[0]
-        self.gameStatus = gameStatus[1]
-        self.gameDate = gameStatus[2]
+        self.finalScore = gameStatus[0] # ' - ' for scheduled games
+        if gameStatus[1].find('(') != -1:
+            self.gameStatus = gameStatus[2] # game ' Terminado ' or ' Agendado ' or real time score
+            ii = 3
+        else:
+            self.gameStatus = gameStatus[1]
+            ii = 2
 
+        if len(gameStatus[ii])<26:
+            self.gameDate = gameStatus[ii]
+        else:
+            self.gameDate = gameStatus[ii+1]
+
+
+        if self.gameStatus == ' Terminado ':
+            if int(self.finalScore.split('-')[0]) > int(self.finalScore.split('-')[1]):
+                self.won = 'Home'
+            elif int(self.finalScore.split('-')[0]) == int(self.finalScore.split('-')[1]):
+                self.won = 'Draw'
+            else:
+                self.won = 'Away'
+            
     def getStats(self,gameWebsite):
         
         tab_content = gameWebsite.find('div', class_="tab_content")
@@ -81,13 +99,22 @@ class game():
         if market0:
             self.faceOff_numTips = market0.find('span', class_="tipsred").text #number of tips made on face off 1x2
             faceOff_container = market0.find_all('tr', class_="even")
-            faceOffStats=[]
-            for stat in faceOff_container:
-                faceOffStats.append(stat.find_all('td'))
+            faceOffStats= [stat.find_all('td')  for stat in faceOff_container]
 
             self.probHome = faceOffStats[0][1].text
             self.probDraw = faceOffStats[1][1].text
             self.probAway = faceOffStats[2][1].text
+
+            probHome = int(self.probHome.split('%')[0])
+            probDraw = int(self.probDraw.split('%')[0])
+            probAway = int(self.probAway.split('%')[0])
+
+            if probHome > probDraw and probHome > probAway:
+                self.toWin = 'Home'
+            elif probDraw > probHome and probDraw > probAway:
+                self.toWin = 'Draw'
+            else:
+                self.toWin = 'Away'
 
             try:
                 self.oddHome = faceOffStats[0][3].span.text
@@ -98,17 +125,14 @@ class game():
                 self.oddDraw = "-"
                 self.oddAway = "-"
         
-
-
-    
-
-        
 if __name__ == "__main__":
     
     data = {}
     data['Games'] = []
     matches = []
     urls = getGameUrls(matches)
+    counter = 0
+    rightCounter = 0
 
     for url in urls:
         gamez = game(url)
@@ -117,8 +141,25 @@ if __name__ == "__main__":
         except :
             continue
         data = writeJson(data, gamez)
+        try: 
+            gamez.won
+        except :
+            continue
+        
+        if int(gamez.faceOff_numTips) > 10:
+            if gamez.toWin == gamez.won:
+                rightCounter += 1
+
+            counter += 1
+        
+
+
     
-    with open('data.txt', 'w', encoding='utf8') as outfile:
-          json.dump(data, outfile, indent=2, ensure_ascii=False)
+    print(f'{counter} games played, {rightCounter} guesses right. {(rightCounter/counter)*100}% accuracy.')
+
+
+    
+    # with open('data.txt', 'w', encoding='utf8') as outfile:
+    #       json.dump(data, outfile, indent=2, ensure_ascii=False)
 
     
